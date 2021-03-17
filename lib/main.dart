e805a57521adc9ce5911faa2b8e80ac8e4ec8318
saurabhxd <srv.skr.099@gmail.com>
2021-03-17 21:43:54 +0530

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 import 'quiz_brain.dart';
 
 QuizBrain quizBrain = QuizBrain();
@@ -28,29 +29,37 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-
-  List<Icon> scoreKeeper = [
-
-  ];
+  List<Icon> scoreKeeper = [];
 
   void checkAnswer(bool userPickedAnswer) {
-    bool correctAnswer = quizBrain.getQuestionAnswer();
+    bool correctAnswer = quizBrain.getCorrectAnswer();
 
     setState(() {
+      if (quizBrain.isFinished() == true) {
+        Alert(
+          context: context,
+          title: 'Finished!',
+          desc: 'You\'ve reached the end of the quiz.',
+        ).show();
 
-    if(userPickedAnswer == correctAnswer) {
-      scoreKeeper.add(
-          Icon(
-          Icons.check,
-          color: Colors.green
+        quizBrain.reset();
+        scoreKeeper = [];
+      }
+
+      else {
+        if (userPickedAnswer == correctAnswer) {
+          scoreKeeper.add(Icon(
+            Icons.check,
+            color: Colors.green,
           ));
-    } else {
-      scoreKeeper.add(
-          Icon(Icons.close,
-          color: Colors.red
+        } else {
+          scoreKeeper.add(Icon(
+            Icons.close,
+            color: Colors.red,
           ));
-    }
-      quizBrain.nextQuestion();
+        }
+        quizBrain.nextQuestion();
+      }
     });
   }
 
